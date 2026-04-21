6a801e77be324650143413ff3496fc0ffdf9505a
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-04-21 16:08:01 -0500

    Added validation to database conection

--- modules/db/db.py
+++ modules/db/db.py
@@ -8,3 +8,12 @@ url=os.getenv("SUPABASE_URL")
 key=os.getenv("SUPABASE_KEY")
 
 supabase = create_client(url, key)
+
+try:
+    response = supabase.table("training_data").select("*").limit(1).execute()
+
+    if response.data is not None:
+        print("Conectado correctamente a la db.")
+
+except Exception as e:
+    print("Error de conexion a la db.", str(e))
