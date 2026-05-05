2d31d7ba7372854ee257ad6a5362fd41025f3f4a
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-05-05 12:07:43 -0500

    feat: add save exmaple request class, respomsible to save signs examples

--- presentation/api/schemas.py
+++ presentation/api/schemas.py
@@ -2,9 +2,16 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
+#prediccion
 class PredictResquest(BaseModel):
     l: List[float] = Field(..., min_items=63, max_items=63)
 
 class PredictResponse(BaseModel):
     l: Optional[str]
     c: float
+
+#guardar ejemplo
+class SaveExampleRquest(BaseModel):
+    letter: str
+    landmrks: List[float] = Field(..., min_items=63, max_items=63)
+
