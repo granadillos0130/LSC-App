1a5dc00069172d4254332bd6a311b1587e34d138
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-05-05 13:30:51 -0500

    feat: add endpoints and function responsible to comunicate client to api and management of data

--- presentation/api/routes.py
+++ presentation/api/routes.py
@@ -19,7 +19,7 @@ from domain.classifier import SignClassifier
 
 rotuer = APIRouter(prefix="/api")
 
-#composición
+#composición: composition root conecta los modulos de la app
 
 repo = DatabaseRepository()
 dataset_service = DatasetService(repo)
@@ -27,3 +27,52 @@ dataset_service.load()
 
 training_service = TrainingService(dataset_service)
 classifier = SignClassifier(dataset_service)
+
+#predict endpoint
+
+@router.post("/predict", response_model=PredictResponse)
+def predict(req: PredictRequest):
+    try:
+        letter, confidence = classifier.predict(req.l)
+
+        return {
+            "l": letter,
+            "c":confidence
+        }
+    except Exception as e:
+        raise HTTPException(500, str(e))
+    
+#example endpoint
+@router.post("/examples")
+def save_example(req: SaveExampleRequest):
+    try:
+        dataset_service.save_example(req.landmarks, req.letter)
+        return {
+            "status":"ok"
+        }
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+#endpoint training batch
+
+@router.post("/training/batch", response_model=BatchResponse)
+def save_batch(data: List[BatchItem]):
+    try:
+        batch = [item.dict() for item in data]
+        training_service.save_batch(batch)
+        return {
+            "saved batch":len(batch)
+        }
+    except Exception as e:
+        raise HTTPException(500, str(e))
+
+#endpoint monitoreo/debug
+
+@router.get("/dataset", response_model=DatasetInfoResponse)
+def dataset_info():
+    try:
+        return{ 
+            "samples":len(dataset_service.data)
+        }
+    except Exception as e:
+        raise(500, str(e))
