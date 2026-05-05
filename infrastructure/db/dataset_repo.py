24273dd5c6b04adc6aa0d6d235c3ea71d4cd4efa
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-05-05 11:39:17 -0500

    feat: add save_batch function, responsible for save data batch whit multiple signs, to training the project

--- infrastructure/db/dataset_repo.py
+++ infrastructure/db/dataset_repo.py
@@ -9,10 +9,8 @@ class DatabaseRepository:
     def save(self, item):
         supabase.table("training_data").insert(item).execute()
 
-    def insert_batch(self, examples, batch_size=500):
+    def save_batch(self, examples, batch_size=500):
         try:
-            for i in range(0, len(examples), batch_size):
-                batch = examples[i:i + batch_size]
-                supabase.table("training_data").insert(batch).execute()
+            supabase.table("training_data").insert(items).execute()
         except Exception as e:
             print(f"Error insertando datos: {e}")    
